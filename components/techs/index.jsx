b96bba65d5fbc62ddbc1171463f066b9d0243e77
CaduSaless <cadubss2@gmail.com>
2026-03-06 20:19:51 -0300

    fix image names

--- components/techs/index.jsx
+++ components/techs/index.jsx
@@ -5,15 +5,15 @@ export default function Techs() {
   const frameWorks = [
     {URL: "/next-icon.svg", alt: "Next", URL_black: "/next-icon-black.svg"},
     {URL: "/React-icon.svg", alt: "React", URL_black: "/React-icon.svg"},
-    {URL: "/Vue-icon.svg", alt: "Vue", URL_black: "/Vue-icon.svg"},
+    {URL: "/vue-icon.svg", alt: "Vue", URL_black: "/vue-icon.svg"},
     {URL: "/node-icon.svg", alt: "Node", URL_black: "/node-icon-black.svg"},
     {URL: "/flask-icon.svg", alt: "Flask", URL_black: "/flask-icon-black.svg"},
     
   ]
 
    const Infra = [
-    {URL: "/Docker-icon.svg", alt: "Docker", URL_black: "/Docker-icon.svg"},
-    {URL: "/Vercel.svg", alt: "Vercel", URL_black: "/vercel-icon-black.svg"},
+    {URL: "/docker-icon.svg", alt: "Docker", URL_black: "/docker-icon.svg"},
+    {URL: "/vercel.svg", alt: "Vercel", URL_black: "/vercel-icon-black.svg"},
     {URL: "/git-icon.svg", alt: "Git", URL_black: "/git-icon.svg"},
     {URL: "/github-icon.svg", alt: "GitHub", URL_black: "/github-icon-black.svg"},
     {URL: "/Cloudflare-icon.svg", alt: "Cloudflare", URL_black: "/Cloudflare-icon.svg"},
