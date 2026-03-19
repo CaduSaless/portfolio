1a87eba18e6c204f488951610739db5fefcecdca
CaduSaless <cadubss2@gmail.com>
2026-03-19 15:44:31 -0300

    Baixar CV changes

--- components/hero/index.jsx
+++ components/hero/index.jsx
@@ -2,6 +2,7 @@ import './style.css'
 import { BackgroundRippleEffect } from '@/components/ui/backgroun-ripple-effect'
 import RevealItem from '@/components/reveal/'
 import Link from 'next/link'
+import { Download } from 'lucide-react';
 
 export default function Hero() {
   return(
@@ -18,9 +19,10 @@ export default function Hero() {
       </div>
       <RevealItem yOffset={0} duration={.7} delay={.2}>
         <div className='flex gap-4 justify-center items-center'>
-        <div className='relative text-background hover:scale-105 hover:bg-azul not-dark:hover:bg-[#bf9aca] hover:text-foreground duration-300 z-40 border hover:shadow   mt-4 border-foreground not-dark:border-0 dark:border-white/20 backdrop-blur-[1px] rounded-2xl bg-white not-dark:bg-escura-2'>
+        <a href="./arquivos/CV.pdf"  download className='relative text-background hover:scale-105 hover:bg-azul not-dark:hover:bg-[#bf9aca] hover:text-foreground duration-300 z-40 border hover:shadow   mt-4 border-foreground not-dark:border-0 dark:border-white/20 backdrop-blur-[1px] rounded-2xl bg-white not-dark:bg-escura-2'>
+          <Download size={20} />
           <button className='uppercase rounded-2xl p-2 px-4 block bg-transparent text-xs '>Baixar CV</button>
-        </div>
+        </a>
         <Link href={"https://wa.me/5567993354035?text=Olá%20Carlos,%20vi%20seu%20portfólio%20e%20gostaria%20de%20conversar!"} className='relative hover:scale-105 hover:bg-white/10 duration-150 z-40 border hover:shadow mt-4 border-foreground dark:border-white/20 backdrop-blur-[1px] rounded-2xl bg-white/5'>
           <p className='uppercase rounded-2xl p-2 block bg-transparent text-xs '>Contatar-me &#9997;</p>
         </Link>
